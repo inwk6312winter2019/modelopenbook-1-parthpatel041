282c5c021d246215b3070cbdc64b58a4e40e589a
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-06 09:48:18 -0800

    task1

--- model1.py
+++ model1.py
@@ -3,10 +3,24 @@ f=f.read()
 f=f.split("\n")
 new_list = list()
 if_list = list()
+vlencheck = 0
 
 def list_ifname_ip():
 	for value in f:
 		if "interface" in value:
 			value=value.split()
 			if_list.append(value[1])
+                if "nameif" in value:
+			value=value.split()
+			if "no" in value[0]:
+				if_list.append("null")
+			else:
+				if_list.append(value[1])
+		if "ip address" in value:
+			if vlancheck == 0:
+				if_list.append("null")
+			value=value.split()
+                else:
+	               if_list.append(value[2])
+print(list_ifname_ip())
 		
