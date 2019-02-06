9c45b7e595243c71843c87381c1e6d5bc64791a9
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-06 08:25:49 -0800

    task-1 continue

--- model1.py
+++ model1.py
@@ -1,6 +1,12 @@
 f=open("running-config.cfg",'r')
 f=f.read()
 f=f.split("\n")
-final_list = list()
-interface_list = list()
-intdict=dict()
+new_list = list()
+if_list = list()
+
+def list_ifname_ip():
+	for value in f:
+		if "interface" in value:
+			value=value.split()
+			if_list.append(value[1])
+		
